0de342db0d1a402f9e736ccc36c8e02a911c0426
Matt Ao <mattyayoh@gmail.com>
2016-02-13 13:47:09 -0500

    Home view controller, clean up the skeleton

---
+++ LAX/HomeViewController.m
@@ -0,0 +1,39 @@
+//
+//  HomeViewController.m
+//  LAX
+//
+//  Created by Matthew Ao on 2/13/16.
+//  Copyright © 2016 AYOH. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+   [super viewDidLoad];
+   self.view.backgroundColor = [UIColor colorWithRed:(193.0/255.0) green:(11.0/255.0) blue:(33.0/255.0) alpha:1.0];
+   
+   // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+   [super didReceiveMemoryWarning];
+   // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
