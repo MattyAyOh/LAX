0b4786a742a6ae5cedd439674d691c6c7fd4de7d
Matt Ao <mattyayoh@gmail.com>
2016-02-18 21:31:45 -0500

    Finishing up the Availability Table View

--- LAX/AvailabilityTableViewController.m
+++ LAX/AvailabilityTableViewController.m
@@ -7,12 +7,18 @@
 //
 
 #import "AvailabilityTableViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface AvailabilityTableViewController ()
 
-@property IBOutlet UIView *myView;
-@property (strong, nonatomic) IBOutlet UIView *Room1;
-@property (strong, nonatomic) IBOutlet UILabel *Room1String;
+@property (strong, nonatomic) IBOutlet UIView *roomOneView;
+@property (strong, nonatomic) IBOutlet UILabel *roomOneLabel;
+@property (strong, nonatomic) IBOutlet UIView *roomTwoView;
+@property (strong, nonatomic) IBOutlet UILabel *roomTwoLabel;
+@property (strong, nonatomic) IBOutlet UIView *roomThreeView;
+@property (strong, nonatomic) IBOutlet UILabel *roomThreeLabel;
+@property (strong, nonatomic) IBOutlet UIView *roomFourView;
+@property (strong, nonatomic) IBOutlet UILabel *roomFourLabel;
 
 @end
 
@@ -21,14 +27,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    [self.view setBackgroundColor:[UIColor colorWithRed:(193.0/255.0) green:(11.0/255.0) blue:(33.0/255.0) alpha:1.0]];
-   [[self Room1] setBackgroundColor:[UIColor redColor]];
-   [self.Room1String setText:@"HEY"];
-   
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+   [[self roomOneView] setBackgroundColor:[UIColor redColor]];
+   self.roomOneLabel.attributedText = [[NSAttributedString alloc] initWithString:@"OCCUPIED SINCE: 10:30PM" attributes:@{ NSStrokeColorAttributeName : [UIColor blackColor], NSForegroundColorAttributeName : [UIColor blackColor], NSStrokeWidthAttributeName : @-5.0 }];
+   self.roomTwoLabel.attributedText = [[NSAttributedString alloc] initWithString:@"AVAILABLE" attributes:@{ NSStrokeColorAttributeName : [UIColor blackColor], NSForegroundColorAttributeName : [UIColor whiteColor], NSStrokeWidthAttributeName : @-5.0 }];
+   self.roomThreeLabel.attributedText = [[NSAttributedString alloc] initWithString:@"AVAILABLE" attributes:@{ NSStrokeColorAttributeName : [UIColor blackColor], NSForegroundColorAttributeName : [UIColor whiteColor], NSStrokeWidthAttributeName : @-5.0 }];
+   self.roomFourLabel.attributedText = [[NSAttributedString alloc] initWithString:@"AVAILABLE" attributes:@{ NSStrokeColorAttributeName : [UIColor blackColor], NSForegroundColorAttributeName : [UIColor whiteColor], NSStrokeWidthAttributeName : @-5.0 }];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -43,10 +46,11 @@
 -(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
    UIView *tempView=[[UIView alloc]initWithFrame:CGRectMake(0,200,300,244)];
+   tempView.backgroundColor=[UIColor clearColor];
    
    UILabel *tempLabel=[[UILabel alloc]initWithFrame:CGRectMake(15,0,300,44)];
    tempLabel.backgroundColor=[UIColor clearColor];
-   tempLabel.shadowColor = [UIColor blackColor];
+   tempLabel.shadowColor = [UIColor colorWithRed:(33.0/255.0) green:(33.0/255.0) blue:(33.0/255.0) alpha:1.0];
    tempLabel.shadowOffset = CGSizeMake(0,2);
    tempLabel.textColor = [UIColor whiteColor]; //here you can change the text color of header.
    tempLabel.font = [UIFont fontWithName:@"Helvetica" size:20.0];
@@ -74,70 +78,15 @@
    return tempView;
 }
 
-//#pragma mark - Table view data source
-//
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Incomplete implementation, return the number of sections
-//    return 0;
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//#warning Incomplete implementation, return the number of rows
-//    return 0;
-//}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+   UITableViewCell *tempCell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
+   
+   [tempCell.contentView.layer setBorderColor:[UIColor blackColor].CGColor];
+   [tempCell.contentView.layer setBorderWidth:2.0f];
+   
+   return tempCell;
+   
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
