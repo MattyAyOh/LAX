e05f74992ed970cdfc5e0e9e53ef282c3175a4f3
Matt Ao <mattyayoh@gmail.com>
2016-02-21 23:18:07 -0500

    Abstract outlets into the base availability class

--- LAX/MahjongTableViewController.h
+++ LAX/MahjongTableViewController.h
@@ -10,21 +10,4 @@
 
 @interface MahjongTableViewController : AvailabilityTableViewController
 
-@property (strong, nonatomic) IBOutlet UIView *roomOneView;
-@property (strong, nonatomic) IBOutlet UILabel *roomOneLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomTwoView;
-@property (strong, nonatomic) IBOutlet UILabel *roomTwoLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomThreeView;
-@property (strong, nonatomic) IBOutlet UILabel *roomThreeLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomFourView;
-@property (strong, nonatomic) IBOutlet UILabel *roomFourLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomFiveView;
-@property (strong, nonatomic) IBOutlet UILabel *roomFiveLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomSixView;
-@property (strong, nonatomic) IBOutlet UILabel *roomSixLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomSevenView;
-@property (strong, nonatomic) IBOutlet UILabel *roomSevenLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomEightView;
-@property (strong, nonatomic) IBOutlet UILabel *roomEightLabel;
-
 @end
--- LAX/PoolTableViewController.h
+++ LAX/PoolTableViewController.h
@@ -10,13 +10,4 @@
 
 @interface PoolTableViewController : AvailabilityTableViewController
 
-@property (strong, nonatomic) IBOutlet UIView *roomOneView;
-@property (strong, nonatomic) IBOutlet UILabel *roomOneLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomTwoView;
-@property (strong, nonatomic) IBOutlet UILabel *roomTwoLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomThreeView;
-@property (strong, nonatomic) IBOutlet UILabel *roomThreeLabel;
-@property (strong, nonatomic) IBOutlet UIView *roomFourView;
-@property (strong, nonatomic) IBOutlet UILabel *roomFourLabel;
-
 @end
